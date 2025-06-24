a1213de30d1eb6b08c56579992af8270c6f5dafc
KN-98 <Nawaf.e.Omair@gmail.com>
2025-06-24 10:13:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 include ('session_check.php');
-  ?>
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -31,7 +31,7 @@ include ('session_check.php');
 </select>
 
 <label for="contract-selector">Select Contract:</label>
-<select id="contract-selector">
+<select id="contract-selector" name="form_contract">
   <option value="S14-842">S14-842</option>
   <option value="Y01-842">Y01-842</option>
   <option value="Y01A-842">Y01A-842</option>
@@ -39,33 +39,33 @@ include ('session_check.php');
 
 <!-- START PAR FORM -->
 <div id="start-par" class="form-section" style="display: block;">
-  <form>
+  <form action="submit_start_par.php" method="post" enctype="multipart/form-data">
     <h3>Start PAR Form</h3>
     <div class="row">
-      <div><label>PAR No:<input type="text"></label></div>
-      <div><label>Date:<input type="date"></label></div>
+      <div><label>PAR No:<input type="text" name="par_no"></label></div>
+      <div><label>Date:<input type="date" name="date"></label></div>
     </div>
-    <label>ID No:<input type="text"></label>
-    <label>Position Title:<input type="text" value="PEST CONTROLLER"></label>
-    <label>Full Name:<input type="text"></label>
-    <label>Nationality:<input type="text"></label>
-    <label>Employee Number:<input type="text"></label>
-    <label>Effective Date:<input type="date"></label>
+    <label>ID No:<input type="text" name="id_no"></label>
+    <label>Position Title:<input type="text" name="position_title" value="PEST CONTROLLER"></label>
+    <label>Full Name:<input type="text" name="full_name"></label>
+    <label>Nationality:<input type="text" name="nationality"></label>
+    <label>Employee Number:<input type="text" name="employee_number"></label>
+    <label>Effective Date:<input type="date" name="effective_date"></label>
     <label>Supervisory Position:
-      <select onchange="toggleDirectorApproval(this, 'director-approval-start')">
+      <select name="is_supervisory" onchange="toggleDirectorApproval(this, 'director-approval-start')">
         <option value="No">No</option>
         <option value="Yes">Yes</option>
       </select>
     </label>
-    <label>Attachment:<input type="file"></label>
+    <label>Attachment:<input type="file" name="attachment"></label>
     <h4>Approval Signatures</h4>
     <div class="row">
-      <div><label>Project Manager:<input type="text"></label></div>
-      <div><label>Contract Engineer:<input type="text"></label></div>
-      <div><label>Group Manager:<input type="text"></label></div>
+      <div><label>Project Manager:<input type="text" name="project_manager"></label></div>
+      <div><label>Contract Engineer:<input type="text" name="contract_engineer"></label></div>
+      <div><label>Group Manager:<input type="text" name="group_manager"></label></div>
     </div>
     <div class="row" id="director-approval-start" style="display: none;">
-      <div><label>Department Director:<input type="text"></label></div>
+      <div><label>Department Director:<input type="text" name="department_director"></label></div>
     </div>
     <button type="submit">Submit Start PAR</button>
   </form>
@@ -73,26 +73,26 @@ include ('session_check.php');
 
 <!-- STOP PAR FORM -->
 <div id="stop-par" class="form-section">
-  <form>
+  <form action="submit_stop_par.php" method="post">
     <h3>Stop PAR Form</h3>
-    <label>Employee Name:<input type="text"></label>
-    <label>Employee Number:<input type="text"></label>
-    <label>Reason for Stop:<textarea></textarea></label>
-    <label>Effective Date:<input type="date"></label>
-    <label>Remarks:<textarea></textarea></label>
+    <label>Employee Name:<input type="text" name="employee_name"></label>
+    <label>Employee Number:<input type="text" name="employee_number"></label>
+    <label>Reason for Stop:<textarea name="reason"></textarea></label>
+    <label>Effective Date:<input type="date" name="effective_date"></label>
+    <label>Remarks:<textarea name="remarks"></textarea></label>
     <label>Supervisory Position:
-      <select onchange="toggleDirectorApproval(this, 'director-approval-stop')">
+      <select name="is_supervisory" onchange="toggleDirectorApproval(this, 'director-approval-stop')">
         <option value="No">No</option>
         <option value="Yes">Yes</option>
       </select>
     </label>
     <h4>Approval Signatures</h4>
     <div class="row">
-      <div><label>Supervisor:<input type="text"></label></div>
-      <div><label>Contract Manager:<input type="text"></label></div>
+      <div><label>Supervisor:<input type="text" name="supervisor"></label></div>
+      <div><label>Contract Manager:<input type="text" name="contract_manager"></label></div>
     </div>
     <div class="row" id="director-approval-stop" style="display: none;">
-      <div><label>Department Director:<input type="text"></label></div>
+      <div><label>Department Director:<input type="text" name="department_director"></label></div>
     </div>
     <button type="submit">Submit Stop PAR</button>
   </form>
@@ -100,34 +100,34 @@ include ('session_check.php');
 
 <!-- VACATION FORM -->
 <div id="vacation" class="form-section">
-  <form>
+  <form action="submit_vacation.php" method="post">
     <h3>Vacation Form</h3>
-    <label>Employee Name:<input type="text"></label>
-    <label>Employee Number:<input type="text"></label>
-    <label>Start Date:<input type="date"></label>
-    <label>Return Date:<input type="date"></label>
-    <label>Total Days:<input type="number"></label>
+    <label>Employee Name:<input type="text" name="employee_name"></label>
+    <label>Employee Number:<input type="text" name="employee_number"></label>
+    <label>Start Date:<input type="date" name="start_date"></label>
+    <label>Return Date:<input type="date" name="return_date"></label>
+    <label>Total Days:<input type="number" name="total_days"></label>
     <label>Type of Leave:
-      <select>
-        <option>Annual</option>
-        <option>Emergency</option>
-        <option>Sick</option>
+      <select name="leave_type">
+        <option value="Annual">Annual</option>
+        <option value="Emergency">Emergency</option>
+        <option value="Sick">Sick</option>
       </select>
     </label>
-    <label>Remarks:<textarea></textarea></label>
+    <label>Remarks:<textarea name="remarks"></textarea></label>
     <label>Supervisory Position:
-      <select onchange="toggleDirectorApproval(this, 'director-approval-vacation')">
+      <select name="is_supervisory" onchange="toggleDirectorApproval(this, 'director-approval-vacation')">
         <option value="No">No</option>
         <option value="Yes">Yes</option>
       </select>
     </label>
     <h4>Approval Signatures</h4>
     <div class="row">
-      <div><label>Project Manager:<input type="text"></label></div>
-      <div><label>Contract Supervisor:<input type="text"></label></div>
+      <div><label>Project Manager:<input type="text" name="project_manager"></label></div>
+      <div><label>Contract Supervisor:<input type="text" name="contract_supervisor"></label></div>
     </div>
     <div class="row" id="director-approval-vacation" style="display: none;">
-      <div><label>Department Director:<input type="text"></label></div>
+      <div><label>Department Director:<input type="text" name="department_director"></label></div>
     </div>
     <button type="submit">Submit Vacation Request</button>
   </form>
@@ -148,9 +148,5 @@ include ('session_check.php');
   }
 </script>
 
- 
-
 </body>
 </html>
-
- 
